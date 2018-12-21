bed88540ec4738fc3b4634fa6d0a3c57f0042ace
Christie Navarrete <christie.navarrete@cima-software.com>
2018-12-21 15:07:28 -0500

    Stress Test with WINSCP

---
+++ SFTP-StressTest/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinSCP;
+
+namespace SFTP_StressTest
+{
+  public  class Configuration
+    {
+        public SessionOptions session() {
+            // Configuración de opciones de sesión
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Sftp,
+                HostName = "Write the ip",
+                UserName = "",
+                Password = "",
+                SshHostKeyFingerprint ="",  // key Finger public generated start with "ecdsa-sha2"
+
+            };
+            return sessionOptions;
+        }
+        
+
+    }
+}
---
+++ SFTP-StressTest/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using WinSCP;
+
+namespace SFTP_StressTest
+{
+    class Program
+    {
+        public static int contadorsftp = 0;
+        public class ChristieObj
+        {
+           // public int contadorsftp;
+            public int xCnt;
+            public ManualResetEvent xEvent;
+        }
+        public static void SessionSFTP(object pEvent)
+        {
+            try
+            {
+                Configuration configuration = new Configuration();
+                SessionOptions sessionOP = configuration.session();
+
+                using (Session session = new Session())
+                {
+                    // Conexión
+                    session.Open(sessionOP);
+                    Random rnd = new Random();
+                    ChristieObj xObjC = (ChristieObj)pEvent;
+                    string RemoteDirectory = "/"; // or can be  root path and directory example '/Log/'"
+                    //TESTWinscp is the first part of file name will save in the remote path, next is the id and the date
+                    string remoteFTP = RemoteDirectory + "TESTWinscp - " + xObjC.xCnt.ToString() + "-" + DateTime.Now.ToString("yyyyMMddTHHmmssfff") + ".txt"; 
+                    session.PutFiles("StressTestFile.txt", remoteFTP);
+                    session.Close();
+                    Console.WriteLine("Done: " + remoteFTP);
+                    xObjC.xEvent.Set();
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                contadorsftp++;
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stress test  :)");
+            Console.WriteLine("Enter interaction number:");
+            int tasksnumber = Int32.Parse(Console.ReadLine());
+            var events = new List<ManualResetEvent>();
+          
+            for (int i=1; i<= tasksnumber; i++)
+            {
+           
+                var resetEvent = new ManualResetEvent(false);
+                ChristieObj xEvent = new ChristieObj();
+                xEvent.xCnt = i;
+                xEvent.xEvent = resetEvent;
+                events.Add(resetEvent);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(SessionSFTP), xEvent);
+            }
+            Console.WriteLine("Workers sent!");
+
+            var wait = true;
+            while (wait)
+            {
+                WaitHandle.WaitAll(events.Take(64).ToArray());
+                events.RemoveRange(0, events.Count > 63 ? 64 : events.Count);
+                wait = events.Any();
+            }
+
+            Console.WriteLine("Workers done!");
+            Console.WriteLine("Errors  : " + contadorsftp);
+            Console.WriteLine("Made with Love, by Christie");
+            Console.WriteLine("Press a key to finish...");
+            Console.ReadKey();
+        }
+
+    }
+}
